6a0c31e1b299716cfda1592bfcff0d7839824088
Yadhukrishanan KV <yadhukrishnan0718@gmail.com>
2025-05-16 19:13:57 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,31 +1,28 @@
-import mongoose, { Mongoose } from "mongoose";
-import { Monofett } from "next/font/google";
+import mongoose from 'mongoose';
 
-let cached = global.mongoose
+let cached = global.mongoose;
 
-if (!cached){
-    cached = global.mongoose = { conn: null, promise: null}
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
 
-    if (cached.conn){
-        return cached.conn
-    }
-    if (!cached.promise){
-        const opts ={
-            bufferCommands:false
-        }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
 
-        cached.promise = Mongoose.connect('${process.env.MONGODB_URI}/quickcart',opts).then( mongoose => {
-        return mongoose
-        })
+    cached.promise = mongoose
+      .connect(`${process.env.MONGODB_URI}/quickcart`, opts)
+      .then((mongoose) => mongoose);
+  }
 
-
-    }
-    
-    cached.conn = await cached.promise
-    return cached.conn
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
-export default connectDB
+export default connectDB;
